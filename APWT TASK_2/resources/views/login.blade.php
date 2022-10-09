fa685dd90bb42785b43557b626fd5c5737112bb8
Anika-Saba-Ibte-Sum <anikasaba15@gmail.com>
2022-10-09 22:29:12 +0600

    Lab Task 2  First Commit

--- APWT TASK_2/resources/views/login.blade.php
+++ APWT TASK_2/resources/views/login.blade.php
@@ -23,13 +23,5 @@
     <input type="submit" class="btn btn-success" value="Sign Up" >                  
 </form>
 
-@if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
+
 @endsection 
